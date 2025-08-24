f8e31aaeb671343abbb137e977c735ac8029de21
Israel Duff <israel-duff@Apples-MacBook-Pro.local>
2025-08-24 15:00:07 +0100

    fixed build issue

--- src/app/lexicon/page.tsx
+++ src/app/lexicon/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import { LexiconPage } from '@/components/lexicon-page';
 
 export default function Home() {
-  return <LexiconPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LexiconPage />
+    </Suspense>
+  );
 }
